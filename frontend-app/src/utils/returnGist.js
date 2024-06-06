7bfbf1b9416c6ac15efc3c5a8000340790098b09
omen <paulk198@hotmail.com>
2024-06-06 10:44:58 +0200

    first commit

--- frontend-app/src/utils/returnGist.js
+++ frontend-app/src/utils/returnGist.js
@@ -1,5 +1,3 @@
 import { Octokit } from "@octokit/rest"
 
-export const octokit = new Octokit({
-  auth: "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",
-})
+export const octokit = new Octokit({})
