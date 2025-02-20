43c740fcac026565c46f3bb1c4908b8fabe8324b
Paul Khoza <68129319+psk-98@users.noreply.github.com>
2025-02-20 20:18:52 +0200

    Update page.jsx
    update landing page meta description

--- frontend-app/src/app/page.jsx
+++ frontend-app/src/app/page.jsx
@@ -7,5 +7,5 @@ export default function Home() {
 export const metadata = {
   title: "Paul Khoza | Website Software Developer, Frontend, Backend",
   description:
-    "Paul Khoza is a proficient software developer specializing in creating websites. Discover more about his contributions to the development of software for the web.",
+    "Meet Paul Khoza, a skilled software developer passionate about building websites. Explore his journey and contributions to web development.",
 }
