a9a7a4488ec66b62ffac293e4a66a53d9bacc4e5
Paul <paul@vetro.co.za>
2026-01-15 20:02:05 +0200

    trying to fix form style issue

--- frontend-app/src/components/contactForm/ContactForm.jsx
+++ frontend-app/src/components/contactForm/ContactForm.jsx
@@ -1,85 +1,84 @@
-"use client"
+"use client";
 
-import { useForm } from "@formspree/react"
-import { useState } from "react"
-import FormCode from "../formCode/FormCode"
-import ThankYouText from "../thankYouText/ThankYouText"
-import styles from "./ContactForm.module.css"
+import { useForm } from "@formspree/react";
+import { useState } from "react";
+import FormCode from "../formCode/FormCode";
+import ThankYouText from "../thankYouText/ThankYouText";
+import styles from "./ContactForm.module.css";
 
 export default function ContactForm() {
-  const [state, handleSubmit, reset] = useForm(
-    process.env.NEXT_PUBLIC_FORMSPREE_ID
-  )
+	const [state, handleSubmit, reset] = useForm(
+		process.env.NEXT_PUBLIC_FORMSPREE_ID,
+	);
 
-  const [inputs, setInputs] = useState({
-    name: "",
-    email: "",
-    message: "",
-  })
+	const [inputs, setInputs] = useState({
+		name: "",
+		email: "",
+		message: "",
+	});
 
-  // const [isSuccess, setSuccess] = useState(false)
+	// const [isSuccess, setSuccess] = useState(false)
 
-  const handleChange = (event) => {
-    event.preventDefault()
-    setInputs((prev) => ({
-      ...prev,
-      [event.target.id]: event.target.value,
-    }))
-  }
+	const handleChange = (event) => {
+		setInputs((prev) => ({
+			...prev,
+			[event.target.id]: event.target.value,
+		}));
+	};
 
-  const handleReset = () => {
-    setInputs({ name: "", email: "", message: "" })
-    reset()
-  }
+	const handleReset = () => {
+		setInputs({ name: "", email: "", message: "" });
+		reset();
+	};
 
-  return (
-    <>
-      <div className={styles.contactFormWrapper}>
-        {state.succeeded ? (
-          <ThankYouText handleReset={handleReset} />
-        ) : (
-          <form className={styles.contactForm} onSubmit={handleSubmit}>
-            <div className={styles.formGroup}>
-              <label>_name:</label>
-              <input
-                id="name"
-                name="name"
-                type="text"
-                required
-                onChange={handleChange}
-                value={inputs.name}
-              />
-            </div>
-            <div className={styles.formGroup}>
-              <label>_email:</label>
-              <input
-                id="email"
-                name="email"
-                type="email"
-                required
-                onChange={handleChange}
-                value={inputs.email}
-              />
-            </div>
-            <div className={styles.formGroup}>
-              <label>_message:</label>
-              <textarea
-                id="message"
-                name="message"
-                required
-                onChange={handleChange}
-                value={inputs.message}
-              />
-            </div>
-            <div className={styles.formGroup}>
-              <button type="submit">
-                {state.submitting ? "submitting.." : "submit-message"}
-              </button>
-            </div>
-          </form>
-        )}
-      </div>
-      <FormCode inputs={inputs} />
-    </>
-  )
+	return (
+		<>
+			<div className={styles.contactFormWrapper}>
+				{state.succeeded ? (
+					<ThankYouText handleReset={handleReset} />
+				) : (
+					<form className={styles.contactForm} onSubmit={handleSubmit}>
+						<div className={styles.formGroup}>
+							<label>_name:</label>
+							<input
+								id="name"
+								name="name"
+								type="text"
+								required
+								onChange={handleChange}
+								value={inputs.name}
+							/>
+						</div>
+						<div className={styles.formGroup}>
+							<label>_email:</label>
+							<input
+								id="email"
+								name="email"
+								type="email"
+								required
+								onChange={handleChange}
+								value={inputs.email}
+							/>
+						</div>
+						<div className={styles.formGroup}>
+							<label>_message:</label>
+							<textarea
+								id="message"
+								name="message"
+								required
+								onChange={handleChange}
+								value={inputs.message}
+							/>
+						</div>
+						<div className={styles.formGroup}>
+							<button type="submit">
+								{state.submitting ? "submitting.." : "submit-message"}
+							</button>
+						</div>
+					</form>
+				)}
+			</div>
+			<FormCode inputs={inputs} />
+		</>
+	);
 }
--- frontend-app/src/components/formCode/FormCode.jsx
+++ frontend-app/src/components/formCode/FormCode.jsx
@@ -1,47 +1,46 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from "react"
-import styles from "./FormCode.module.css"
+import styles from "./FormCode.module.css";
 
 export default function FormCode({ inputs }) {
-  const [codeSnippet, setCodeSnippet] = useState("")
+	if (!inputs) return null;
 
-  useEffect(() => {
-    const snippet = () => (
-      <>
-        <div>
-          <span className="token keyword">const</span> message{" "}
-          <span className="token operator">=</span>{" "}
-          <span className="token punctuation">{`{`}</span>
-        </div>
-        <div style={{ padding: "0 0 0 1rem" }}>
-          <span className="token literal-property property">name</span>
-          <span className="token operator">:</span>{" "}
-          <span className="token string">&quot;{inputs?.name}&quot;</span>,{" "}
-        </div>
-        <div style={{ padding: "0 0 0 1rem" }}>
-          <span className="token literal-property property">email</span>
-          <span className="token operator">:</span>{" "}
-          <span className="token string">&quot;{inputs?.email}&quot; </span>,{" "}
-        </div>
-        <div style={{ padding: "0 0 0 1rem" }}>
-          <span className="token literal-property property">message</span>
-          <span className="token operator">:</span>{" "}
-          <span className="token string" style={{ whiteSpace: "break-spaces" }}>
-            &quot;{inputs?.message}&quot;
-          </span>
-        </div>
-        <span className="token punctuation">{`}`}</span>
-      </>
-    )
-    setCodeSnippet(snippet)
-  }, [inputs])
+	return (
+		<div className={styles.formCodeWrapper}>
+			<pre className="language-js">
+				<code>
+					<div>
+						<span className="token keyword">const</span> message{" "}
+						<span className="token operator">=</span>{" "}
+						<span className="token punctuation">{`{`}</span>
+					</div>
 
-  return (
-    <div className={styles.formCodeWrapper}>
-      <pre className="language-js">
-        <code>{codeSnippet}</code>
-      </pre>
-    </div>
-  )
+					<div style={{ paddingLeft: "1rem" }}>
+						<span className="token literal-property property">name</span>
+						<span className="token operator">:</span>{" "}
+						<span className="token string">&quot;{inputs.name}&quot;</span>,
+					</div>
+
+					<div style={{ paddingLeft: "1rem" }}>
+						<span className="token literal-property property">email</span>
+						<span className="token operator">:</span>{" "}
+						<span className="token string">&quot;{inputs.email}&quot;</span>,
+					</div>
+
+					<div style={{ paddingLeft: "1rem" }}>
+						<span className="token literal-property property">message</span>
+						<span className="token operator">:</span>{" "}
+						<span
+							className="token string"
+							style={{ whiteSpace: "break-spaces" }}
+						>
+							&quot;{inputs.message}&quot;
+						</span>
+					</div>
+
+					<span className="token punctuation">{`}`}</span>
+				</code>
+			</pre>
+		</div>
+	);
 }
